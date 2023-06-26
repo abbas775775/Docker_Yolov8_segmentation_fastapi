362cb6e6ca8afda2e44e838975de3553b605a0d2
amin asghari soureh <61617353+abbas775775@users.noreply.github.com>
2023-06-26 18:11:51 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ import cv2
 import numpy as np
 import ultralytics
 from ultralytics import YOLO
-from ultralytics.yolo.utils.plotting import Annotator, colors #me: annotator uses opencv like cv2.rectangle, cv2.draw.text,......
+from ultralytics.yolo.utils.plotting import Annotator, colors 
 
 app = FastAPI(title='Deploying a ML Model with FastAP')
 
